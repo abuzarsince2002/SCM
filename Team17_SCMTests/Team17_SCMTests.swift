f1d1cbd68ee34c7ed2bac1d2be6a3496e35245a3
Kushagra Kulshrestha <kushagra.22scse1012963@galgotiasuniversity.edu.in>
2024-12-02 11:55:22 +0530

    Initial Commit

---
+++ Team17_SCMTests/Team17_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team17_SCMTests.swift
+//  Team17_SCMTests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import Team17_SCM
+
+struct Team17_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
